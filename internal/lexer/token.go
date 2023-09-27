a2c5add8ae4885ba9e9722f285fe1ef09f7f9b3f
Milos Zivlak <zivlakmilos@gmail.com>
2023-09-27 05:12:25 +0200

    Token

---
+++ internal/lexer/token.go
@@ -0,0 +1,44 @@
+package lexer
+
+type TokenType int
+
+const (
+	TokenTypeIllegal = iota
+	TokenTypeEof
+	TokenTypeInt
+	TokenTypeHex
+	TokenTypeBin
+	TokenTypeNop
+	TokenTypeLdA
+	TokenTypeLdB
+	TokenTypeStA
+	TokenTypeStB
+	TokenTypeSetA
+	TokenTypeSetB
+	TokenTypeNot
+	TokenTypeAnd
+	TokenTypeOr
+	TokenTypeXor
+	TokenTypeNand
+	TokenTypeNor
+	TokenTypeAdd
+	TokenTypeSub
+	TokenTypeJmp
+	TokenTypeJmpZ
+	TokenTypeJmpC
+	TokenTypeHlt
+)
+
+type Token struct {
+	tokenType TokenType
+	literal   string
+}
+
+func NewToken(tokenType TokenType, literal string) *Token {
+	token := Token{
+		tokenType: tokenType,
+		literal:   literal,
+	}
+
+	return &token
+}
